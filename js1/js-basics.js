c40b30cc07ad466a0402db83475622f8ef63dda7
Harrisonp365 <harrisonpowell365@gmail.com>
2021-09-20 11:19:12 +1000

    some of the morning content

---
+++ js1/js-basics.js
@@ -0,0 +1,29 @@
+// JS types
+typeof 15; // Prediction: {your answer here} int
+typeof 5.5; // Prediction: ... int(double)
+typeof NaN; // Prediction: int
+typeof "hello"; // Prediction: string
+typeof true; // Prediction: bool
+typeof (1 != 2); // Prediction: bool
+"hamburger" + "s"; // Prediction: hamburgerss
+"hamburgers" - "s"; // Prediction: NaN
+"1" + "3"; // Prediction: 13
+"1" - "3"; // Prediction: -2
+"johnny" + 5; // Prediction: johnny5
+"johnny" - 5; // Prediction: NaN
+99 * "luftbaloons"; // Prediction: NaN
+
+const myString = "hello world";
+let myInt = 25;
+const myBool = 1;
+let myNewString = myString + " world string";
+myNewString = "Hi World";
+
+// const true = "harry";
+
+const firstName = "Harrison";
+const lastName = "Powell";
+const age = 25;
+
+console.log(firstName + " " + lastName);
+console.log("Age: " + age);
