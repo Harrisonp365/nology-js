6f149bc1ee0ef109d98f176c34614eb69774a25d
Harrisonp365 <harrisonpowell365@gmail.com>
2021-09-21 12:19:16 +1000

    challenge two working on control flow statements

--- js1/js-basics.js
+++ js1/js-basics.js
@@ -85,31 +85,48 @@
 
 //Booleans
 
-console.log(parseInt("Roisin"));
-
-2 == "2"; //returns true
-2 === 2; // returns true
-2 === "2"; //returns false
-"2" + 3 == 23; // returns true (string + 3 = 23 ==23)
-"2" + 3 === 5; // returns false
-10 % 3; // returns 1 3 goes into 10 3 times remainder 1
-10 % 3 === 1; // returns true
-(100 % 7) % 5; // 7 goes into 100 14 times r 2 (2 % 5) just = 2 as 2 is remainder
-(100 % 7) % 5 !== 0; // returns true
-(100 % 7) % 9 == 2; // returns true
-!(10 % 2); // 10 % 2 == 0 but negate the 0 equals 1 == true
-!!!!((10 % 7) % 3); // (10 % 7 = 3) (3 % 3 = 0) !!!!0 = false
-10 % 3 === -1 % 2; // (10 % 3 = 1) (-1 % 2 = -1) returns false // console logged equations
-(892783 != "89278" + 3) == 0; // (892783 != "89278" + 3) = false == 0 returns true
-true && false; // false
-false || true; // true
-true || false; // true
-true || false; // true
-true || false + true; // true
-true * false && false + true; // returns 0 or false? falsey value of 0 converted into ints?
-10 && 123 && -1 && 3; // all values are truthey so returns 3 (needs to read all ints)
-10 && 123 && 0 && 3; // returns 0 as falsey value
-(10 && (123 || "") && parseInt("Roisin")) || 23 / 23 - 1;
+// console.log(parseInt("Roisin"));
+//
+// 2 == "2"; //returns true
+// 2 === 2; // returns true
+// 2 === "2"; //returns false
+// "2" + 3 == 23; // returns true (string + 3 = 23 ==23)
+// "2" + 3 === 5; // returns false
+// 10 % 3; // returns 1 3 goes into 10 3 times remainder 1
+// 10 % 3 === 1; // returns true
+// (100 % 7) % 5; // 7 goes into 100 14 times r 2 (2 % 5) just = 2 as 2 is remainder
+// (100 % 7) % 5 !== 0; // returns true
+// (100 % 7) % 9 == 2; // returns true
+// !(10 % 2); // 10 % 2 == 0 but negate the 0 equals 1 == true
+// !!!!((10 % 7) % 3); // (10 % 7 = 3) (3 % 3 = 0) !!!!0 = false
+// 10 % 3 === -1 % 2; // (10 % 3 = 1) (-1 % 2 = -1) returns false // console logged equations
+// (892783 != "89278" + 3) == 0; // (892783 != "89278" + 3) = false == 0 returns true
+// true && false; // false
+// false || true; // true
+// true || false; // true
+// true || false; // true
+// true || false + true; // true
+// true * false && false + true; // returns 0 or false? falsey value of 0 converted into ints?
+// 10 && 123 && -1 && 3; // all values are truthey so returns 3 (needs to read all ints)
+// 10 && 123 && 0 && 3; // returns 0 as falsey value
+// (10 && (123 || "") && parseInt("Roisin")) || 23 / 23 - 1;
 // had to break down problem 2nd half of && = 0 first = 123 so returns 0 as falsey value
-3 && "Calum" && ("" || 26); // final truthey val is 26 so returns 26
-3 && "Remi" && (null || 86 * 0 || ("" + 1 && "Cat" + "Dog")); //same as above read though all and return final truthey val of "CatDog"
+// 3 && "Calum" && ("" || 26); // final truthey val is 26 so returns 26
+// 3 && "Remi" && (null || 86 * 0 || ("" + 1 && "Cat" + "Dog")); //same as above read though all and return final truthey val of "CatDog"
+
+// Control flow
+
+const price1 = 5;
+const max = 10;
+
+if (max >= price1) {
+  console.log(max);
+}
+
+const n = 12;
+
+if (n % 2 == 0) {
+  console.log("n is even");
+} else {
+  console.log("n is odd");
+}
