44e3e04352272cf02038cb2952052855b365f76e
Harrisonp365 <harrisonpowell365@gmail.com>
2021-09-22 13:09:52 +1000

    up to lunch time wednesday

--- js1/js-basics.js
+++ js1/js-basics.js
@@ -146,7 +146,7 @@
 // } else {
 //   console.log("invalid contents");
 // }
-
+//-------------------------------------------------------------------------------------------------------
 // const errorCode = 0;
 
 // switch (errorCode) {
@@ -169,3 +169,102 @@
 // }
 
 //FUNCTIONS BRUZ
+
+// function myFunc() {
+//   x = 5;
+
+//   return x;
+// }
+
+// let myNum = myFunc();
+
+// console.log(myNum);
+
+// function rectArea(width, length) {
+//   return width * length;
+// }
+
+// let myRect = rectArea(10, 5);
+
+// console.log(myRect);
+
+//Challenges
+// MVP
+// Write a function that takes in a number of earth days, and returns the number of orbits the moon can perform given days.
+// moonOrbits(54) => 2
+// moonOrbits(365) => 13.359
+// Challenge: Working with Circles
+// MVP
+// Write a function that takes the radius of a circle and return its area.
+// Write a function that takes the radius of a circle and return its perimeter.
+// circleArea(10) => 314.16
+// circlePerimeter(10) => 62.83
+// Challenge: Years to Days & Seconds
+// MVP
+// Create a function that takes your age in years and returns your age in days.
+// Create a function that takes your age in years and returns your age in seconds.
+// ageInDays(32) => 11680
+// ageInSeconds(32) => 1009152000
+
+// const moonOrbit = 27;
+
+// function moonOrbits(days) {
+//   let orbitsOfMoonGivenDays = days / moonOrbit;
+//   return orbitsOfMoonGivenDays;
+// }
+// console.log(moonOrbits(54));
+
+// function circleArea(radius) {
+//   let area = null; //check to see if you can init a val first
+//   area = radius ** 2 * Math.PI;
+//   return area;
+// }
+// console.log(circleArea(10));
+
+// function circlePerim(radius) {
+//   let perim = 2 * Math.PI * radius;
+//   return perim;
+// }
+// console.log(circlePerim(10));
+
+// function ageInDays(yearsOld) {
+//   return yearsOld * 365;
+// }
+// console.log(ageInDays(25));
+
+// function ageInSeconds(yearsOld) {
+//   let secsInDay = 24 * 60 * 60;
+//   return ageInDays(yearsOld) * secsInDay;
+// }
+// console.log(ageInSeconds(25));
+
+// const circAreaArrowFunc = (radius) => {
+//   const area = Math.PI * radius ** 2;
+//   return area;
+// };
+
+// circAreaArrowFunc(10);
+// console.log(circAreaArrowFunc.area);
+
+// const myFunc = () => {
+//   const fName = "Harry";
+//   return fName;
+// };
+
+// const myFunc2 = () => {
+//   const lName = "Powell";
+//   const firstName = myFunc();
+//   console.log(firstName);
+//   return lName;
+// };
+
+// console.log(myFunc);
+// console.log(myFunc2);
+
+let birdArr = ["Mango", "Squeaky", "grape"];
+
+function feedMyBorb(birdArr) {
+  console.log(birdArr + " gets fed");
+}
+
+feedMyBorb(birdArr[2]);
