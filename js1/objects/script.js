2a2494d3cdecd847f6ab6645124977c1aafe80c2
Harrisonp365 <harrisonpowell365@gmail.com>
2021-09-29 12:22:32 +1000

    finished up aidans lesson

--- js1/objects/script.js
+++ js1/objects/script.js
@@ -8,20 +8,30 @@ const car = {
     color: "silver",
     wheels: 4,
 };
+// console.log(car[0]); // this wont work unless its in an array
 
 let keys = Object.keys(car);
-console.log(keys);
+// console.log(keys);
 
-keys.push("petrolType"); // this will only push to new copy called keys
+// keys.push("petrolType"); // this will only push to new copy called keys
 let carValues = Object.values(car);
-
-car.color = "blue";
-car.wheels = 3;
-
-
 
 keys.map((key, i) => {
     if(car[key] !== carValues[i]) {
         console.log("The car has changed");
+    }
+});
+
+car.brand = "Mazda";
+car.clutch = "yes";
+let carEntries = Object.entries(car);
+
+carEntries.map((kvp, i) => {
+    if(kvp[0] !== keys[i]) {
+        console.log("The key has changed");
+    }
+
+    if(kvp[1] !== carValues[i]) {
+        console.log("The value has changed");
     }
 });
