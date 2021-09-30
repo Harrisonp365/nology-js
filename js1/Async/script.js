9b905fd495a22e5a90d6d0bf87c4d5c98bcaeb67
Harrisonp365 <harrisonpowell365@gmail.com>
2021-09-30 12:00:12 +1000

    blocking and non blocking

--- js1/Async/script.js
+++ js1/Async/script.js
@@ -7,6 +7,35 @@
 //Vocab
 //promise
 //synchronous, asynchronous
-//callback
+//callback -> func to be called at some point in the future
 //event
+
+//Blocking & Non-Blocking
+//Blocking/synchronous -> starting a process and waiting to do anything before process ends
+//Non-blocking/asynchronous -> startings a process and being able to do other things while process is happening
+
+//Blocking code
+let count = 10;
+count += 2;
+console.log(count);
+
+//Non-Blocking code / Async
+const button = document.querySelector("#btn");
+
+button.addEventListener("click", (event) => {
+    alert('clicked!!')
+});
+
+console.log("I happen before button was clicked");
+
+//Non-Blocking code / Async
+setTimeout(() => {
+    alert("I happen in 3 seconds");
+}, 3000);
+
+console.log("I happen before the time out callback");
+
+//------------------------------------------------------------------------------------------------------
+
+//PROMISES
 
