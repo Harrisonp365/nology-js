fdc79cf355c4691550c2f1e91a2d3e0b17b16c75
Harrisonp365 <harrisonpowell365@gmail.com>
2021-10-05 12:08:36 +1100

    jest

---
+++ js1/testing/script.js
@@ -0,0 +1,22 @@
+//Why do we need to test?
+//How?
+//What is Jest?
+//What is babel?
+//TDD
+//Why is TDD useful?
+
+//Why?
+//Make sure everything works correctly
+//Define function behaviour
+//So we can make changes sefely later
+//Forces us to write pure functions more reusable, not relying on program state
+
+//How?
+//Using Jest (javascript test)
+//Just allows use to run test files
+//describe, test , it => struct of the test files
+//expect, assertions, making sure functions have the right output
+
+//Babel
+//javascript has lots of run times
+//browser run times, 
